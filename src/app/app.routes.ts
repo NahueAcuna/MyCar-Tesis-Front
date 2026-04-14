64c400b5e93b98d49bb6f64faee1b1cd52953f19
Franco <francolopezdeboca12@gmail.com>
2026-04-13 21:35:45 -0300

    Cree pagina del detalle de la publicacion. Falta funcionalidad y logica.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './Components/user-layout/home/home';
+import { PublicationDetail } from './pages/publication-detail/publication-detail';
+
 
 export const routes: Routes = [
-    {path:'', component:Home}
+    {path:'', component:Home},
+    {path:'publication/:id', component:PublicationDetail}
 ];
