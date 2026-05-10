94b657875149db7b45b9620d04f77bd8e0278814
Franco <francolopezdeboca12@gmail.com>
2026-05-10 16:23:56 -0300

    a

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,28 +1,59 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RegisterRequest } from '../../models/RegisterRequest';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
-  user: RegisterRequest = {
-    nombre: '',
-    email: '',
-    telefono: '',
-    password: ''
-  };
 
-  constructor(private authService: AuthService) {}
+  registerForm : FormGroup;
+
+  constructor(private authService: AuthService, private fb : FormBuilder, private router: Router) {
+    this.registerForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30), Validators.pattern('^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$')]],
+      email: ['', [Validators.required, Validators.email]],
+      telefono: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]]
+    });
+  }
+
+  get nombre(){
+    return this.registerForm.get('nombre');
+  }
+
+  get email(){
+    return this.registerForm.get('email');
+  }
+
+  get telefono(){
+    return this.registerForm.get('telefono');
+  }
+
+  get password(){
+    return this.registerForm.get('password');
+  }
 
   register(){
-    this.authService.register(this.user).subscribe({
+    this.authService.register(this.registerForm.value).subscribe({
       next: (response) => {console.log('Registro exitoso', response); alert('Registro exitoso');},
-      error: (error) => {console.error('Error en el registro', error); alert('Error en el registro');}
+      error: (error) => {console.error(error);
+        if(error.status === 409){
+          alert('El email ya está registrado');
+        }else{
+          alert('Error en el registro');
+        }
+      }
     });
+
+    this.registerForm.reset();
+  }
+
+  goBack() {
+    this.router.navigate(['/']);
   }
 }
