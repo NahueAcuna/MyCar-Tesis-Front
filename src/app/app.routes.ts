810c1549d2309025a1b42e76b732937983b9c1b5
Franco <francolopezdeboca12@gmail.com>
2026-05-11 20:34:18 -0300

    Listo login. Falta Interceptor JWT, Guards y Perfil Usuario.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { PublicationDetail } from './pages/publication-detail/publication-detail
 import { PublicationForm } from './pages/publication-form/publication-form';
 import { Register } from './pages/register/register';
 import { GestionAdmin } from './pages/gestion-admin/gestion-admin';
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'vender', component: PublicationForm },
     { path: 'publicacion/:id', component: PublicationDetail },
     { path: 'registro', component: Register },
+    { path: 'login', component: Login },
     { path: 'gestionar', component: GestionAdmin }
 ];
---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { Header } from '../../Components/user-layout/header/header';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth-service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [Header, ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  loginForm : FormGroup;
+  hidePassword: boolean = true;
+
+  constructor(private authService: AuthService, private fb : FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  get email(){
+    return this.loginForm.get('email');
+  }
+
+  get password(){
+    return this.loginForm.get('password');
+  }
+
+  login(){
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (response) => {
+        console.log('Login exitoso', response);
+        this.authService.saveToken(response.token);
+        this.authService.saveUser({
+          id: response.id,
+          nombre: response.nombre,
+          email: response.email,
+          rol: response.rol
+        });
+        alert('Login exitoso');
+        this.router.navigate(['/']);  
+      },
+      error: (error) => {console.error(error);
+        if(error.status === 401){
+          alert('Email o contraseña incorrectos');
+        }else{
+          alert('Error al iniciar sesión.');
+        }
+      }
+    });
+  }
+
+  togglePassword() {
+    this.hidePassword = !this.hidePassword;
+  }
+}
