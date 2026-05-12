6fa49f82b86f912aa263bc92c938bcbfa27d5d1f
Franco <francolopezdeboca12@gmail.com>
2026-05-12 14:37:13 -0300

    Agregue jwt interceptor y guards. Falta todas las rutas, perfil y verificar si anda lo de los guards.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { PublicationForm } from './pages/publication-form/publication-form';
 import { Register } from './pages/register/register';
 import { GestionAdmin } from './pages/gestion-admin/gestion-admin';
 import { Login } from './pages/login/login';
+import { authGuard } from './guards/auth-guard';
+import { adminGuard } from './guards/admin-guard';
 
 export const routes: Routes = [
     { path: '', component: Home },
-    { path: 'vender', component: PublicationForm },
-    { path: 'publicacion/:id', component: PublicationDetail },
+    { path: 'vender', component: PublicationForm, canActivate: [authGuard] },
+    { path: 'publicacion/:id', component: PublicationDetail, canActivate: [authGuard] },
     { path: 'registro', component: Register },
     { path: 'login', component: Login },
-    { path: 'gestionar', component: GestionAdmin }
+    { path: 'gestionar', component: GestionAdmin, canActivate: [adminGuard] }
 ];
