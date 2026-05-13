ef1486fa6b0e090a6da4b1a61965dcb6fc2136bd
Nahuel <nahuelacuna426@gmail.com>
2026-05-13 19:48:13 -0300

    Ya funcionan los filtros pero todavia no se puede filtar por categoria y faltan otros detalles visuales y de esperiencia de usuario

--- src/app/Components/user-layout/inventory/inventory.ts
+++ src/app/Components/user-layout/inventory/inventory.ts
@@ -2,11 +2,12 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { PublicationResponse } from '../../../models/PublicationResponse';
 import { PublicationService } from '../../../services/publication-service';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inventory',
-  imports: [FormsModule],
+  imports: [CommonModule,FormsModule, ReactiveFormsModule],
   templateUrl: './inventory.html',
   styleUrl: './inventory.css',
 })
@@ -25,8 +26,32 @@ export class Inventory implements OnInit{
 
   @ViewChild('searchInput') searchInput!: ElementRef;
 
-  constructor(private publicationService: PublicationService, public router : Router){
+  filtersForm: FormGroup;
 
+  made: FormControl;
+  model: FormControl;
+  minPrice: FormControl;
+  maxPrice: FormControl;
+  minKm: FormControl;
+  maxKm: FormControl;
+
+
+  constructor(private publicationService: PublicationService, public router : Router){
+    this.made = new FormControl('');
+    this.model = new FormControl('');
+    this.minPrice = new FormControl(0);
+    this.maxPrice = new FormControl(50000);
+    this.minKm = new FormControl('');
+    this.maxKm = new FormControl('');
+
+    this.filtersForm = new FormGroup({
+      made: this.made,
+      model: this.model,
+      minPrice: this.minPrice,
+      maxPrice: this.maxPrice,
+      minKm: this.minKm,
+      maxKm: this.maxKm
+    })
   }
 
   ngOnInit(): void {
@@ -94,6 +119,46 @@ actualizarMinimo(event: any) {
 
 actualizarMaximo(event: any) {
   this.precioMaximo = Number(event.target.value);
+}
+applyFilters() {
+  const filters = this.filtersForm.value;
+
+  this.filteredPublications = this.publications.filter(p => {
+    const car = p.auto;
+
+    if (filters.made && car.marca.toLowerCase() !== filters.made.toLowerCase()) {
+      return false; 
+    }
+   
+    if (filters.model && car.modelo.toLowerCase() !== filters.model.toLowerCase()) {
+      return false;
+    }
+    
+    if (filters.minPrice != null && car.precio < filters.minPrice) {
+      console.log(filters.minPrice)
+      console.log("auto: " + car.precio)
+
+      return false;
+    }
+    
+    if (filters.maxPrice != null && car.precio > filters.maxPrice) {
+      console.log("hola")
+
+      return false;
+    }
+
+    if (filters.minKm != null && car.km < filters.minKm) {
+      console.log("hola")
+      return false;
+    }
+
+    if (filters.maxKm != null && filters.maxKm != "" && car.km > filters.maxKm) {
+      console.log("hola")
+      return false;
+    }
+    return true; 
+  });
+  console.log(this.filteredPublications)
 }
 
 }
