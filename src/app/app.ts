4c7c54b6e65783f0da0548a643f9c2d79084c8d7
FedericoMeniy <federicomeniy@gmail.com>
2026-08-07 17:23:56 -0300

    Arreglo de sintaxis

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,17 +9,17 @@ import { Footer } from './Components/footer/footer';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements AfterViewInit, OnDestroy{
+export class App implements AfterViewInit, OnDestroy {
   protected readonly title = signal('tesis-Front');
   private lenis: any;
   private reqId: any;
 
-  constructor(private ngZone: NgZone) {}
+  constructor(private ngZone: NgZone) { }
 
   ngAfterViewInit(): void {
     this.ngZone.runOutsideAngular(() => {
       this.lenis = new Lenis({
-        duration: 1.2, 
+        duration: 1.2,
         easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
         smoothWheel: true,
       });
